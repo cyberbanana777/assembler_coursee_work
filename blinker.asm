3b288a5e6410f3581a72b7a230cd2389f9f931d1
Alexander Grachev <sashagrachev2005@gmail.com>
2025-09-05 17:09:35 +0300

    Added features. Now had control of RGB-led

--- blinker.asm
+++ blinker.asm
@@ -7,57 +7,91 @@
 .def counter2 = r18
 .def counter3 = r19
 
-; Pinout
-.edu red_led = 0    ; First analog pin 
-.edu green_led = 1  ; Second analog pin
-.edu yellow_led = 2 ; Third analog pin
+.def red_led_state = r20
+.def green_led_state = r21
+.def blue_led_state = r22
+.def state = r23
 
-; setup input|output mode
-.edu ANALOG_PINS_CONFIG = 0b00000111
-.edu DIGIT_PINS_CONFIG = 0b00000000
-
-; setup pin state voltage
-.edu PIN_STATE_OUTPUT 0b00000000 
-; setup pin mode (None|Input_pull_up)
-.edu PIN_MODE_INPUT 0b00001100
-
-; Начало программы (точка входа)
+; Начало программы
 .cseg
-.org 0x0000 ; Указываем, что код начинается с начала памяти программ
+.org 0x0000
 
-    ; Настройка стека (очень важно для работы подпрограмм!)
-    ldi temp, low(RAMEND) ; Младший байт конца RAM
-    out SPL, temp         ; в указатель стека SPL
-    ldi temp, high(RAMEND); Старший байт конца RAM
-    out SPH, temp         ; в указатель стека SPH
+    ; Настройка стека
+    ldi temp, low(RAMEND)
+    out SPL, temp
+    ldi temp, high(RAMEND)
+    out SPH, temp
 
-    out DDRC, ANALOG_PINS_CONFIG  ; setup analog pins
-    out PORTC, PIN_STATE_OUTPUT   ; set voltage in output pins
+    ; Настройка пинов D5 и D6 как выходов
+    ldi temp, 0b01100000  ; D6 и D5 как выходы, остальные как входы
+    out DDRD, temp
 
-    out DDRD, DIGIT_PINS_CONFIG   ; setup digital (0-7) pins
-    out PORTD, PIN_MODE_INPUT     ; enable input_pull_up for input pins
+    ; Настройка пина D9 как выхода
+    ldi temp, 0b00000010  ; D9 как выход
+    out DDRB, temp
 
+    ; Настройка ТАЙМЕРА0
+    ldi temp, (1<<COM0A1)|(1<<COM0B1)|(1<<WGM01)|(1<<WGM00)
+    out TCCR0A, temp
+    ldi temp, (1<<CS01)|(1<<CS00)
+    out TCCR0B, temp
+
+    ; Настройка ТАЙМЕРА1
+    ldi temp, (1<<COM1A1)|(1<<WGM10)
+    sts TCCR1A, temp
+    ldi temp, (1<<WGM12)|(1<<CS11)|(1<<CS10)
+    sts TCCR1B, temp
+
+    ; init state
+    ldi state, 0
+
+    ; Инициализация всех каналов
+    ldi red_led_state, 0
+    ldi green_led_state, 0
+    ldi blue_led_state, 0
+    rcall update_leds
+
+    rcall delay_1s
+    rcall delay_1s
+ 
+    rjmp main_loop
 
 ; Главный бесконечный цикл
 main_loop:
-   sbi PORTB, 5           ; Включаем светодиод
-   rcall delay_1s         ; Вызываем задержку 1 секунда
+    sbi PORTB, 5           ; Включаем светодиод на D13
+    ldi red_led_state, 0   ; Красный выключен
+    ldi green_led_state, 255 ; Зеленый выключен
+    ldi blue_led_state, 0  ; Синий выключен
+    rcall update_leds
+    rcall delay_1s
 
-   cbi PORTB, 5           ; Выключаем светодиод
-   rcall delay_1s         ; Вызываем задержку 1 секунда
-   rjmp main_loop         ; Бесконечный цикл
+    cbi PORTB, 5           ; Выключаем светодиод на D13
+    ldi red_led_state, 128   ; Красный на половину
+    ldi green_led_state, 0   ; Зеленый выключен
+    ldi blue_led_state, 200    ; Синий выключен
+    rcall update_leds
+    rcall delay_1s
+
+    rjmp main_loop
 
 
-; Подпрограмма задержки ~1 секунда при 16 МГц
+; Обновление светодиодов
+update_leds:
+    out OCR0B, green_led_state  ; D5 = Зеленый
+    out OCR0A, red_led_state    ; D6 = Красный
+    sts OCR1AL, blue_led_state  ; D9 = Синий
+    ret
+
+; Подпрограмма задержки ~1 секунда
 delay_1s:
-    ldi  counter1, 130    ; 1 такт
-    ldi  counter2, 222    ; 1 такт
-    ldi  counter3, 224    ; 1 такт
+    ldi counter1, 130
+    ldi counter2, 222
+    ldi counter3, 224
 delay_loop:
-    dec  counter3         ; 1 такт
-    brne delay_loop       ; 2 такта (1 при последней итерации)
-    dec  counter2         ; 1 такт
-    brne delay_loop       ; 2 такта (1 при последней итерации)
-    dec  counter1         ; 1 такт
-    brne delay_loop       ; 2 такта (1 при последней итерации)
-    ret                   ; 4 такта
+    dec counter3
+    brne delay_loop
+    dec counter2
+    brne delay_loop
+    dec counter1
+    brne delay_loop
+    ret
